0a4d54dfe42d75f98615306e59a5529d72b94305
Alex Kadai <alexandrkadai@gmail.com>
2023-04-27 16:47:36 +0300

    MealBack firebase configured

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,18 @@
+import { useEffect } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://test12-a8f65-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+      const responseData = await response.json();
+      const loadedMeals 
+    };
+    fetchMeals();
+  }, []);
+
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
