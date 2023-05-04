c5bf9f860ba82e5492821fc4849aee5f1717fe6a
Alex Kadai <alexandrkadai@gmail.com>
2023-05-04 22:34:23 +0300

    End of day

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -5,14 +5,12 @@ const isEmpty = (value) => value.trim().length === '';
 const isNotPostal = (value) => value.trim().lenght !== 5;
 
 const Checkout = (props) => {
-
   const [formInputVal, setFormInputVal] = useState({
     name: false,
     street: true,
     postal: true,
     city: true,
   });
-
 
   const nameInputRef = useRef();
   const streetInputRef = useRef();
@@ -32,17 +30,17 @@ const Checkout = (props) => {
     const enteredPostalIsValid = isNotPostal(enteredPostal);
 
     setFormInputVal({
-        name: enteredNameIsValid,
-        street: enteredStreetIsValid,
-        city: enteredCityIsValid,
-        postal:enteredPostalIsValid
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postal: enteredPostalIsValid,
     });
 
     const formIsValid =
       enteredNameIsValid && enteredStreetIsValid && enteredCityIsValid && enteredPostalIsValid;
 
     if (!formIsValid) {
-        console.log('Not Succsesssdcsf');
+      console.log('Not Succsesssdcsf');
       return;
     }
     if (formIsValid) {
@@ -52,7 +50,7 @@ const Checkout = (props) => {
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={`${classes.control} ${formInputVal.name ? '' : classes.invalid}`}>
+      <div className={`${classes.control} ${formInputVal.name ? '' : classes.invalidß}`}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
         {!formInputVal.name && <p>Please Enter Valid Name</p>}
@@ -60,17 +58,17 @@ const Checkout = (props) => {
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
-         {!formInputVal.street && <p>Please Enter Valid street</p>}
+        {!formInputVal.street && <p>Please Enter Valid street</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
         <input type="number" id="postal" ref={postalInputRef} />
-         {!formInputVal.postal && <p>Please Enter Valid postal</p>}
+        {!formInputVal.postal && <p>Please Enter Valid postal</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
-         {!formInputVal.city && <p>Please Enter Valid city</p>}
+        {!formInputVal.city && <p>Please Enter Valid city</p>}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
