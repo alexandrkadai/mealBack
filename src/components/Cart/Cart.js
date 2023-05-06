d645fb4b3515912d86877d790f10ea2ce6f4d194
Alex Kadai <alexandrkadai@gmail.com>
2023-05-06 21:56:14 +0300

    SomeTExt

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -39,19 +39,18 @@ const Cart = (props) => {
     </ul>
   );
 
-  const buttonModal =
-  <div className={classes.actions}>
-  <button className={classes['button--alt']} onClick={props.onClose}>
-    Close
-  </button>
-  {hasItems && (
-    <button className={classes.button} onClick={orderHandler}>
-      Order
-    </button>
-  )}
-</div>
-;
-
+  const buttonModal = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -60,7 +59,7 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       {!isEmpty ? <CheckOut onCancel={props.onClose} /> : <p>Nothing</p>}
-      {isEmpty ?buttonModal : ''}
+      {isEmpty ? buttonModal : ''}
     </Modal>
   );
 };
